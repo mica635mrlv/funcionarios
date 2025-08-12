d6fb1b33c943f36c3a69c89e4a7cb7d12197ab48
mica635mrlv <millaregina635@gmail.com>
2025-08-12 11:11:15 -0300

    projeto em andamento 12/08

---
+++ app.js
@@ -0,0 +1,49 @@
+'use stript'
+
+// // import funcionarios from "./funcionarios.json" with {type: "json"}
+
+// // console.log (funcionarios)
+
+// const funcionarios = [
+//     {
+//         url: './img/ana.jpg',
+//         nome: 'Maria Souza',
+//         cargo: 'Designer'
+//     },
+//     {
+//         imagem: './img/joao.jpg',
+//         nome: 'João Silva',
+//         cargo: 'DBA'
+//     },
+//     {
+//         imagem: './img/hugo.jpg',
+//         nome: 'Hugo Pereira',
+//         cargo: 'FrontEnd'
+//     },
+//     {
+//         imagem: './img/ana.jpg',
+//         nome: 'Ana Maria',
+//         cargo: 'BackEnd'
+//     }
+// ]
+
+// function criarPerfil (srcImagem) {
+//     const container = document.getElementById(container)
+//     const imagem = document.getElementById('img')
+//     const nome = document.getElementById('span')
+//     const cargo = document.getElementById('p')
+
+//     imagem.src = srcImagem.url
+//     nome.textContent = srcImagem.nome
+//     cargo.textContent = srcImagem.cargo
+
+//     container.appendChild(imagem)
+//     container.appendChild(nome)
+//     container.appendChild(cargo)
+// }
+
+// function carregarPerfis() {
+//     funcionarios.forEach(criarPerfil)
+// }
+
+// carregarPerfis()
